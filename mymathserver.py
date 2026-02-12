72db707aaa699233e6ff654dd29d862309b93d95
Sai Ashwin <e1553621@u.nus.edu>
2026-02-12 14:25:21 +0800

    Update server port configuration
    Change default port to 5001 and allow it to be set via environment variable.

--- mymathserver.py
+++ mymathserver.py
@@ -1,23 +1,13 @@
-# python mymathserver.py
-
-
-
+import os
 from connexion import FlaskApp
-
-
 
 app = FlaskApp(__name__)
 app.add_api('mymathserver.yml')
 
-
-
 @app.route('/')
 def index():
-
-	return 'Welcome to My Math Server'
-
-
+    return 'Welcome to My Math Server'
 
 if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", "5001"))  # default 5001
+    app.run(host='0.0.0.0', port=port)
